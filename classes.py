c8a396039187e7e2b3ecb0a0fecf0a47ade44ca4
unknown <akatadaniel@gmail.com>
2024-11-22 13:42:53 +0100

    classes and files

---
+++ classes.py
@@ -0,0 +1,91 @@
+# we did this on friday 
+# CLASS
+# class blueprintDogs:
+#     instance of a class 
+#     property 
+#     def__init__(self,height,name,color,leg):
+#         self.name = Name
+#         self.height = height
+#         self.color = color
+#         self.leg = leg
+    
+# # method 
+# def bark(self):
+#     return f'my name is {self.name} and i can bark'
+# def run(self):
+#     return f'i saw {self.name} run with is {self.leg} legs'
+
+
+# # create objects - initializing a class object
+# d1 = blueprintDogs (2.5,'bingo','black',4)
+# print(d1.bark())
+# print(d1.run())
+
+
+
+# # inheritance
+# class person:
+#     def__init__(self,height,name,name,age):
+# #         self.name = Name
+# #         self.email = email
+# #         self.leg = age
+#     def greetings (self):
+#         return f'my name is {self.name} i am {self.age},you can mail me {self.email}
+
+# class user (person)
+#     def__init__(self,height,name,email,leg):
+# #         self.name = Name
+# #         self.email = email
+# #         self.age = age
+# #         self.sex = male 
+# #    def gender (self,sex)
+# #        self.sex=sex
+# # 
+# # john = user('zainab', 'john@gmail.com',20)
+# # john.gender ('male')
+# # print(john.sex)       
+
+
+# # classes of class
+# class circle :
+#     pi = 3.142    @classmethod
+#     def value(cls):
+#         return f' a circle has a default pie value = {cls.pi}'
+
+
+# # initialize a cirle object
+# c1 = circle (5)
+
+# print(c1.pi)
+# # print(c1.value())
+
+
+# class planet:
+#     class properties 
+#     shape = "round"
+#     instance properties 
+
+#     def__init__(self,name,radius):
+#         self.name= name 
+#         self.radius= radius
+#     instance method 
+#     def orbit (self)
+#         return f'{self.name} is a good planet'
+    
+#     class method
+#     @classmethod 
+#     def common(cls):
+#         return f' all the planet are {cls.shape} due to gravity'
+
+
+#    initialise an object 
+
+
+#    venus = planet ('venus',3)
+#    print(venus.name)
+#    print(venus.radius)
+#    print(venus.shape)
+#    print(venus.orbit())
+#    print(venus.common())
+#    print(venus.shape)
+#    print(venus.common())
